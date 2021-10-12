ee4a4dceb9aacc8411d1f608f71cb8d4d1c44bb5
MalayP98 <tomalaypandey.tanu@gmail.com>
2021-10-12 20:59:02 +0530

    replaced hardcoded values with constants

--- src/main/java/com/clone/postmanc/users/UserController.java
+++ src/main/java/com/clone/postmanc/users/UserController.java
@@ -1,5 +1,6 @@
 package com.clone.postmanc.users;
 
+import com.clone.postmanc.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api" + "/user")
+@RequestMapping(AppConstants.API_URL + AppConstants.USER_URL)
 public class UserController {
 
   @Autowired
   private UserService userService;
 
-  @PostMapping(value = "/add")
+  @PostMapping(value = AppConstants.ADD_URL)
   // TODO : Validate user.
   public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO userDTO) {
     return ResponseEntity.status(HttpStatus.CREATED).body(userService.addUser(userDTO));
