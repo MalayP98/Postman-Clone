939c94864760b9f65a87978b05321b7c036e362a
MalayP98 <tomalaypandey.tanu@gmail.com>
2021-10-12 20:53:07 +0530

    configured AuthenticationManager

--- src/main/java/com/clone/postmanc/security/AppSecurityConfiguration.java
+++ src/main/java/com/clone/postmanc/security/AppSecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.clone.postmanc.security;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import com.clone.postmanc.users.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,17 +14,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  private UserService userService;
+
   @Bean
   public PasswordEncoder defaultPasswordEncoder() {
     return NoOpPasswordEncoder.getInstance();
   }
 
   @Override
+  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder)
+      throws Exception {
+    authenticationManagerBuilder.inMemoryAuthentication().withUser("test").password("pass")
+        .roles("ADMIN");
+    authenticationManagerBuilder.userDetailsService(userService);
+  }
+
+  @Override
   public void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.authorizeRequests().anyRequest().authenticated();
+    httpSecurity.authorizeRequests().anyRequest().hasAnyRole("USER", "ADMIN");
     httpSecurity.formLogin().disable();
     httpSecurity.httpBasic();
     httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    httpSecurity.csrf().disable();
   }
-
 }
